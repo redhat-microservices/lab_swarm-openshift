7d3d43ea7acc1d16e3da3b1724581cd6364f82f7
Charles Moulliard <cmoulliard@apache.org>
2017-01-24 22:11:52 +0100

    Add missing files

---
+++ cdservice/src/main/java/org/cdservice/rest/NewCrossOriginResourceSharingFilter.java
@@ -0,0 +1,27 @@
+package org.cdservice.rest;
+
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+
+@Provider
+public class NewCrossOriginResourceSharingFilter
+		implements
+			ContainerResponseFilter {
+
+	@Override
+	public void filter(ContainerRequestContext request,
+			ContainerResponseContext response) {
+		response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+		response.getHeaders().putSingle("Access-Control-Expose-Headers",
+				"Location");
+		response.getHeaders().putSingle("Access-Control-Allow-Methods",
+				"GET, POST, PUT, DELETE");
+		response.getHeaders()
+				.putSingle("Access-Control-Allow-Headers",
+						"Content-Type, User-Agent, X-Requested-With, X-Requested-By, Cache-Control");
+		response.getHeaders().putSingle("Access-Control-Allow-Credentials",
+				"true");
+	}
+}
