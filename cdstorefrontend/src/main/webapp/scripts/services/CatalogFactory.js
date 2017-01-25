90129626f59e4f175739a53080f505d225000ea5
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-01-25 10:45:43 +0100

    moved fetching the config to config service

--- cdstorefrontend/src/main/webapp/scripts/services/CatalogFactory.js
+++ cdstorefrontend/src/main/webapp/scripts/services/CatalogFactory.js
@@ -1,19 +1,8 @@
-angular.module('cdservice').factory('CatalogResource', function ($resource,$http) {
-
-  var serviceUrl = $http.get("service.json")
-    .success(function (data) {
-      console.log("Resource : " + data['cd-service'] + ':CatalogId');
-      return data['cd-service'].toString();
-    })
-    .error(function () {
-      console.log('could not find service.json ....');
-    });
-  //var resource = $resource('http://localhost:8080/rest/catalogs/:CatalogId', {CatalogId: '@id'}, {
-  var resource = $resource(serviceUrl + ':CatalogId', {CatalogId: '@id'}, {
+angular.module('cdservice').factory('CatalogResource', function ($resource, config) {
+  return $resource(config.getApiUrl() + ':CatalogId', { CatalogId: '@id' }, {
     'queryAll': {
       method: 'GET',
       isArray: true
-    }, 'query': {method: 'GET', isArray: false}, 'update': {method: 'PUT'}
+    }, 'query': { method: 'GET', isArray: false }, 'update': { method: 'PUT' }
   });
-  return resource;
 });
