2708ca497d4c43b021098c77e6512001ff8d3059
Charles Moulliard <cmoulliard@apache.org>
2017-01-30 12:39:12 +0100

    Test to create the cluster

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
+#
+# The command ./install.sh cluster will tell to the openshift oc client that a new openshift cluster must be started
+# If it exists as docker-machine, then it will be deleted first
+#
+
 set -e
 
-oc cluster up --docker-machine=openshift --version=v1.4.1 --create-machine
-HOST_IP=$(docker-machine ip openshift)
+if [[ -n $1 ]]; then
+  docker-machine rm -y openshift
+  oc cluster up --docker-machine=openshift --version=v1.4.1 --create-machine
+  HOST_IP=$(docker-machine ip openshift)
+  # minishift start --openshift-version=v1.4.1 --memory=4000 --vm-driver=virtualbox --iso-url=https://github.com/minishift/minishift-centos-iso/releases/download/v1.0.0-rc.1/minishift-centos7.iso --docker-env=[storage-driver=devicemapper]
+  # HOST_IP=$(minishift ip)
 
-oc login https://$HOST_IP:8443 -u system:admin
-oc adm policy add-cluster-role-to-user cluster-admin admin
-oc login -u admin -p admin
+  oc login https://$HOST_IP:8443 -u system:admin
+  oc adm policy add-cluster-role-to-user cluster-admin admin
+  echo "OpenShift server created."
+else
+  HOST_IP=$(minishift ip)
+fi
+
+oc login https://$HOST_IP:8443 -u admin -p admin
 oc project default
 
 oc new-project snowcamp
@@ -16,7 +30,7 @@ sleep 5
 
 cd cdstorefrontend
 oc new-build --binary --name=cdfrontend -l app=cdfrontend
-npm install
+#npm install
 oc start-build cdfrontend --from-dir=. --follow
 oc new-app cdfrontend -l app=cdfrontend
 oc env dc/cdfrontend BACKEND_URL=http://cdservice-snowcamp.$HOST_IP.xip.io/rest/catalogs/
