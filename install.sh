e2033fa60bd62c3a7caf2082bcab9d663c818608
Charles Moulliard <cmoulliard@apache.org>
2017-01-30 14:01:52 +0100

    Add test to check if service  replies

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
 #
-# The command ./install.sh cluster will tell to the openshift oc client that a new openshift cluster must be started
+# The command ./install.sh cluster will tell to minishift to create a new VM & install/Deploy docker/openshift
+# openshift oc client that a new openshift cluster must be started
 # If it exists as docker-machine, then it will be deleted first
 #
 
 set -e
 
 if [[ -n $1 ]]; then
-  docker-machine rm -y openshift
-  oc cluster up --docker-machine=openshift --version=v1.4.1 --create-machine
-  HOST_IP=$(docker-machine ip openshift)
+  # docker-machine rm -y openshift
+  # oc cluster up --docker-machine=openshift --version=v1.4.1 --create-machine --use-existing-config --host-config-dir /var/lib/minishift/openshift.local.config --host-data-dir /var/lib/minishift/hostdata
+  # HOST_IP=$(docker-machine ip openshift)
+
+  # Issue with minishift 1.0.0.Beta3
   # minishift start --openshift-version=v1.4.1 --memory=4000 --vm-driver=virtualbox --iso-url=https://github.com/minishift/minishift-centos-iso/releases/download/v1.0.0-rc.1/minishift-centos7.iso --docker-env=[storage-driver=devicemapper]
-  # HOST_IP=$(minishift ip)
+
+  minishift start --memory=4000 --vm-driver=virtualbox
+  HOST_IP=$(minishift ip)
 
   oc login https://$HOST_IP:8443 -u system:admin
   oc adm policy add-cluster-role-to-user cluster-admin admin
@@ -42,3 +47,13 @@ oc expose service cdfrontend
 cd ../cdservice
 mvn clean package
 mvn fabric8:deploy -Popenshift
+
+# Test service
+APP=http://cdservice-snowcamp.$HOST_IP.xip.io/
+
+while [ $(curl --write-out %{http_code} --silent --output /dev/null $APP/rest/catalogs) != 200 ]
+ do
+   echo "Wait till we get http response 200 .... from $APP/rest/catalogs"
+   sleep 20
+done
+echo "SUCCESS !"
