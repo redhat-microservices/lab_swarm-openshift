20b31e7a2911f830c4df93227091bbf6d3dca7b0
Charles Moulliard <cmoulliard@apache.org>
2017-02-01 11:43:51 +0100

    Rename folder

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+#
+# Script responsible to setup the project
+# IT alos allow to install using brew jboss-forge and the addons required
+#
+# command to be used at the root of the project cloned
+# ./setup.sh install-forge
+
 CURRENT=$(pwd)
 
 cd $TMPDIR && rm -rf snowcamp
@@ -17,9 +24,9 @@ forge -e "run $CURRENT/scripts/cdstore-forge.fsh"
 
 echo # ----------------  Book Store Web Front End [:8081/rest] ---------------
 echo # Now we want to create front end swarm service to access CD Catalog Service
-mkdir -p cdstorefrontend/public
+mkdir -p cdfront/public
 
-cp -r $CURRENT/scripts/front/ cdstorefrontend
+cp -r $CURRENT/scripts/front/ cdfront
 
 echo # ---------------- Project created ------------------
 pwd && ls -la
