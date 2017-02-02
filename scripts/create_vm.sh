4bd6dd2b05d25e9bf13dcc9bf466f36664d3664c
Charles Moulliard <cmoulliard@apache.org>
2017-02-02 08:11:52 +0100

    Add option to specify the box & clean

--- scripts/create_vm.sh
+++ scripts/create_vm.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
-# docker-machine rm -y openshift
-# oc cluster up --docker-machine=openshift --version=v1.4.1 --create-machine --use-existing-config --host-config-dir /var/lib/minishift/openshift.local.config --host-data-dir /var/lib/minishift/hostdata
-# HOST_IP=$(docker-machine ip openshift)
+$to_be_clean=${1:-}
+$box=${2:-centos}
 
-# Issue with minishift 1.0.0.Beta3 - https://github.com/minishift/minishift/issues/355
-# ISO_URL=https://github.com/minishift/minishift-centos-iso/releases/download/v1.0.0-beta.1/minishift-centos.iso
-# ISO_URL=https://github.com/minishift/minishift-centos-iso/releases/download/v1.0.0-rc.1/minishift-centos7.iso
-# minishift start --memory=4000 --vm-driver=virtualbox --iso-url=$ISO_URL --docker-env=[storage-driver=devicemapper]
+if [[ $to_be_clean -eq "clean" ]]; then
+  echo "Delete $home/.minishift"
+  rm -rf ~/.minishift
+fi
 
-minishift start --memory=4000 --vm-driver=virtualbox
+  case $box in
+    *"boot2docker"*)
+    minishift start --memory=4000 --vm-driver=virtualbox
+    ;;
+    *"centos"*)
+    # Issue with minishift 1.0.0.Beta3 - https://github.com/minishift/minishift/issues/355
+    # ISO_URL=https://github.com/minishift/minishift-centos-iso/releases/download/v1.0.0-beta.1/minishift-centos.iso
+    ISO_URL=https://github.com/minishift/minishift-centos-iso/releases/download/v1.0.0-rc.1/minishift-centos7.iso
+    minishift start --memory=4000 --vm-driver=virtualbox --iso-url=$ISO_URL --docker-env=[storage-driver=devicemapper]
+    ;;
+esac
+
 HOST_IP=$(minishift ip)
 
 oc login https://$HOST_IP:8443 -u system:admin
 oc adm policy add-cluster-role-to-user cluster-admin admin
-
-echo "OpenShift server created."
